172d74a2bee481128dc1fa97f2d10035139b294b
Evyatar <29202833+Evyatar-Kraus@users.noreply.github.com>
2021-10-15 01:20:32 +0300

    wk2/d5/daily_challenge - little refactor

--- week_2/day_5/daily_challenge/script.js
+++ week_2/day_5/daily_challenge/script.js
@@ -30,9 +30,13 @@ const BOTTLES_ON_THE_WALL = getUserNumberInput(MIN_BEERS_BOTTLES, MAX_BEERS_BOTT
 const BUY_MORE_STR = 'Go to the store and buy some more,\n';
 
 //func to generate the lyric section
-const generateLyricSection = ({currentBottlesUse, bottlesLeft, bottlesLeftAfterThisUse,isOneBottle,
-    noMoreBottles, nextOneIsOneBottle, firstBottle, zeroBottlesMessage, initialBottlesOnTheWall
-    }) => {
+const generateLyricSection = ({bottlesLeft, currentBottlesUse, initialBottlesOnTheWall, bottlesLeftAfterThisUse}) => {
+
+    let zeroBottlesMessage = ZERO_BEERS_DESC;
+    let firstBottle = currentBottlesUse === 1 && bottlesLeft === initialBottlesOnTheWall;
+    let noMoreBottles = bottlesLeft === 0;
+    let isOneBottle = bottlesLeft === 1;
+    let nextOneIsOneBottle = bottlesLeft - currentBottlesUse === 1 && currentBottlesUse === 1;
 
     const LINE_3 = `Take ${currentBottlesUse} down and pass ${firstBottle ? ONE_BOTTLE_PRON : MANY_BOTTLE_PRON} around,\n`;
 
@@ -40,23 +44,18 @@ const generateLyricSection = ({currentBottlesUse, bottlesLeft, bottlesLeftAfterT
     lines[0] = `${noMoreBottles ? zeroBottlesMessage: bottlesLeft } bottle${isOneBottle ?'':'s'} ${LINES_1_4_ENDING},\n`;
     lines[1] = `${noMoreBottles ? zeroBottlesMessage: bottlesLeft } bottle${isOneBottle ?'':'s'} ${LINES_2_ENDING}.\n`;
     lines[2] = `${noMoreBottles? BUY_MORE_STR: LINE_3}`;
-    lines[3] = `${noMoreBottles?  initialBottlesOnTheWall : bottlesLeftAfterThisUse}`
-    lines[3] = lines[3] + ` bottle${nextOneIsOneBottle  ? '':'s'} ${LINES_1_4_ENDING}.`;
+    lines[3] = `${bottlesLeftAfterThisUse === 0 ? ZERO_BEERS_DESC: noMoreBottles  ?  initialBottlesOnTheWall : bottlesLeftAfterThisUse }`;
+    lines[3] = lines[3] + ` bottle${bottlesLeftAfterThisUse=== 1  ? '':'s'} ${LINES_1_4_ENDING}.`;
 
     let msg = lines.join("");
-    return msg
+    return msg;
 }
 
 //keeping all the variables in a state object
 let current_state = {
-    bottlesLeft: BOTTLES_ON_THE_WALL,
-    firstBottle: true,
-    isOneBottle :BOTTLES_ON_THE_WALL === 1,
-    noMoreBottles :BOTTLES_ON_THE_WALL === 0,
-    currentBottlesUse: 1,
-    nextOneIsOneBottle: null,
     bottlesLeftAfterThisUse: BOTTLES_ON_THE_WALL,
-    zeroBottlesMessage:ZERO_BEERS_DESC,
+    bottlesLeft: BOTTLES_ON_THE_WALL,
+    currentBottlesUse: 1,
     initialBottlesOnTheWall:BOTTLES_ON_THE_WALL
 }
 
@@ -64,32 +63,25 @@ function SingBottlesOfBeer(){
 
     //looping
     while(current_state.bottlesLeft - current_state.currentBottlesUse  >= 0){
-       current_state.bottlesLeftAfterThisUse =current_state.bottlesLeft  -current_state.currentBottlesUse;
-       current_state.isOneBottle =current_state.bottlesLeft === 1; //now before use we have 1 bottle
-       current_state.noMoreBottles =current_state.bottlesLeft === 0; //now before use we have 0 bottles
-       current_state.nextOneIsOneBottle =current_state.bottlesLeftAfterThisUse === 1;//after use we will have 1 bottle
-
+        current_state.bottlesLeftAfterThisUse = current_state.bottlesLeft - current_state.currentBottlesUse;
         let msg = generateLyricSection(current_state);
         console.log(msg);
-
-       current_state.bottlesLeft =current_state.bottlesLeftAfterThisUse;
-       current_state.firstBottle = false; //first loop
-       current_state.currentBottlesUse = current_state.currentBottlesUse + 1;
+        current_state.bottlesLeft = current_state.bottlesLeftAfterThisUse;
+        current_state.currentBottlesUse = current_state.currentBottlesUse + 1;
     }
 
     //finishing whatever is left on the wall - so using the bottlesLeftAfterThisUse
-    if(current_state.bottlesLeftAfterThisUse > 0){
+    if(current_state.bottlesLeft > 0){
+        current_state.bottlesLeftAfterThisUse = 0;
         current_state.currentBottlesUse = current_state.bottlesLeft;
-        current_state.isOneBottle = current_state.bottlesLeft === 1;
-        current_state.noMoreBottles = current_state.bottlesLeft === 0;
-        current_state.nextOneIsOneBottle = false;
         let msg = generateLyricSection(current_state);
         console.log(msg);
-
+        current_state.currentBottlesUse = 0;
+        current_state.bottlesLeft = 0;
     }
 
-    current_state.noMoreBottles = true;
     //last message -  my ending to the song
+    current_state.bottlesLeftAfterThisUse = current_state.initialBottlesOnTheWall;
     console.log(generateLyricSection(current_state));
 }
 
